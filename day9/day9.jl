836b508bc3a1e9c6ae65bef92a0569d4e70c4ce9
dustpancake <fergusbkr@gmail.com>
2020-12-09 20:34:58 +0100

    refactoring and benchmarking

--- day9/day9.jl
+++ day9/day9.jl
@@ -1,26 +1,20 @@
 getinput(path::AbstractString="day9/test.txt") = map(x->parse(Int, x), eachline(path))
 
-@inline function checknum(subarr::AbstractArray{Int64}, num::Int)::Bool
-    for (i,v) in enumerate(subarr)
-        if any(findall(==(num-v), subarr) .!= i)
-            return true
-        end
-    end 
-    return false 
-end
-
-function part1(input::AbstractArray{Int64}, mask::Int)::Tuple{Int,Int}
-    for (i, v) in enumerate(view(input, mask+1:length(input))) # using raw for loop over algorithm guaruntees short circuit
-        if !(checknum(view(input, i:i+mask), v)) # views to save memory 
-            return (i+mask, v)
+function part1(input::AbstractArray{Int64}, mask::Int)::Tuple{Int, Int}
+    for (i, v) in enumerate(input[mask + 1: end])
+        subarr = @view input[i:i + mask - 1]
+        if !any(i -> v - i in subarr, subarr)
+            return (i + mask, v)
         end
     end
-    warn("No solution found for part 1.")
+    warn("No solution found.")
     return (-1, -1)
 end
 
 function testcase1()
     input = getinput()
+    @show part1(input, 5)
+
     @assert(part1(input, 5) == (15, 127))
 end
 
@@ -31,7 +25,7 @@ input = getinput("day9/input.txt")
 index, key = part1(input, 25)
 println("Part 1: key=$key, index=$index")
 
-function consecutive(input::AbstractArray{Int64}, goal::Int)
+@inline function consecutive(input::AbstractArray{Int64}, goal::Int)
     partial_sum = 0
     for i in 1:length(input)
         partial_sum += input[i] # partial sum
@@ -58,6 +52,25 @@ function part2(input::AbstractArray{Int64}, mask::Int, key::Int)
     maximum(selection) + minimum(selection)
 end
 
+function part2b(input::AbstractArray{Int64}, key::Int)
+    """ inspired by Tom Kwong's solution """
+    i=1
+    j=2
+    partial_sum = input[i] + input[j]
+    while j < length(input)
+        if partial_sum < key
+            j += 1
+            partial_sum += input[j]
+        elseif partial_sum > key
+            partial_sum -= input[i]
+            i += 1
+        else
+            return i, j, sum(extrema(input[i:j]))
+        end
+    end
+    return -1
+end
+
 function testcase2()
     input = getinput("day9/test.txt")
     @assert(part2(input, 5, 127) == 62)
@@ -68,3 +81,9 @@ testcase2()
 # part 2 
 solution = part2(input, 25, key)
 println("Part 2: sol=$solution")
+
+using BenchmarkTools
+#@btime part2($input, 25, $key)
+#@btime part2b($input, $key)
+
+@btime part1b($input, 25)
