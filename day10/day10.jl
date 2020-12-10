87e66db6f7857536c3da0a53d27d0a87d84572c7
dustpancake <fergusbkr@gmail.com>
2020-12-10 08:57:13 +0100

    small refactor

--- day10/day10.jl
+++ day10/day10.jl
@@ -1,7 +1,4 @@
 using BenchmarkTools
-using Combinatorics
-
-input = map(x->parse(Int, x), eachline("day10/input.txt"))
 
 function diffmap!(input::AbstractArray)::Array{Int, 1}
     sort!(input)
@@ -14,23 +11,18 @@ function part1!(dm::AbstractArray)
     (count(==(1), dm) + 1) * (count(==(3), dm) + 1)
 end
 
-input
-#@btime part1!(diffmap!($input))
-#@show part1!(diffmap!(input))
-
-_tribcache = Int[1, 2, 4]
 function tribonacci(n)::Int
-    n = max(n, 1)
-    if n <= length(_tribcache)
-        return _tribcache[n]
+    """ bare bones implementation """
+    if n <= 1
+        return 1
+    elseif n == 2
+        return 2
+    elseif n == 3 
+        return 4
     else
-        val = tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)
-        push!(_tribcache, val)
-        return _tribcache[n]
+        return tribonacci(n-1) + tribonacci(n-2) + tribonacci(n-3)  
     end
 end
-
-
 
 function part2!(dm::AbstractArray)
     """ using counts of successive ones:                                  permutations
@@ -40,11 +32,11 @@ function part2!(dm::AbstractArray)
         1 1 1 1 3      2 1 1 3, 1 1 2 3, 1 2 1 3, 2 2 3, 1 3 3, 3 1 3     : 7
         1 1 1 1 1 3    ...                                                : 13
         ...
-        tribonacci numbers!
+        looks like tribonacci numbers 
     """
     count = 1
     permuts = 1
-    for (i, v) in enumerate(dm)
+    for v in dm
         if v == 1
             count += 1
         else
@@ -55,4 +47,12 @@ function part2!(dm::AbstractArray)
     permuts * tribonacci(count)
 end
 
+# read in file
+input1 = map(x->parse(Int, x), eachline("day10/input.txt"))
+
+@btime part1!(diffmap!($input))
+
 @btime part2!(diffmap!($input))
+
+println("Part 1 ", part1!(diffmap!(input)))
+println("Part 2 ", part2!(diffmap!(input)))
